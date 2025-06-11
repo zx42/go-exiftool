7c81727a3e95975ee9f615ff8dadce9afdc49386
zx42 <2400037-zx42@users.noreply.gitlab.com>
2025-06-11 22:24:06 +0200

    Add hiding console window

--- platform_darwin.go
+++ platform_darwin.go
@@ -5,3 +5,7 @@ var readyToken = []byte("{ready}\n")
 const writeMetadataSuccessToken = "image files updated\n"
 
 var exiftoolBinary = "exiftool"
+
+func hideConsoleWindow(cmd *exec.Cmd) {
+	//cmd.SysProcAttr = &syscall.SysProcAttr{CreationFlags: 0x08000000} // CREATE_NO_WINDOW
+}
--- platform_windows.go
+++ platform_windows.go
@@ -1,7 +1,16 @@
 package exiftool
 
+import (
+	"os/exec"
+	"syscall"
+)
+
 var readyToken = []byte("{ready}\r\n")
 
 const writeMetadataSuccessToken = "image files updated\r\n"
 
 var exiftoolBinary = "exiftool.exe"
+
+func hideConsoleWindow(cmd *exec.Cmd) {
+	cmd.SysProcAttr = &syscall.SysProcAttr{CreationFlags: 0x08000000} // CREATE_NO_WINDOW
+}
